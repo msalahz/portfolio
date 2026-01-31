6fd01183e90651f010b440f55bf48c5b7f0f2fe3
Mohammed Zaghloul <m.salahz86@gmail.com>
2026-01-31 20:43:16 +0200

    feat(landing): add About, Stats, and Footer sections to main layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { HeroSection } from '@/features/landing/components/HeroSection'
+import { AboutSection } from '@/features/landing/components/AboutSection'
+import { StatsSection } from '@/features/landing/components/StatsSections'
+import { FooterSection } from '@/features/landing/components/FooterSection'
 
 export const Route = createFileRoute('/')({ component: App })
 
@@ -7,6 +10,9 @@ function App() {
   return (
     <div>
       <HeroSection />
+      <AboutSection />
+      <StatsSection />
+      <FooterSection />
     </div>
   )
 }
